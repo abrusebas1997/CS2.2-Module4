3d04c0a636dffecc88238bfee25f90274f80d5e4
Sebastian Abarca <49495659+abrusebas1997@users.noreply.github.com>
2021-07-19 13:38:06 -0700

    Update code.py

--- code.py
+++ code.py
@@ -17,51 +17,62 @@ def build_shortest_path(graph, start):
   #vertices
   #Initial costs, 0 and infinity, math.inf
   #precious, init as empty strings
+  unvisited = []
   for v in graph.keys():
+    unvisited.append(v)
     if v == start:
       table[v] = [0, ""]
     else:
       table[v] = [math.inf, ""]
 
   print(table)
-
-  visited = []
+  print(unvisited)
 
   current = start
   #look at a vertex (start with the given start)
   #look at its neighbors (for loop to look at all neighbors)
-  neighbors = graph[current]
-  print("Current: ", current)
-  print("Neighbors", neighbors)
 
-  for n in neighbors:
-    if n[0] not in visited:
-      #update costs of neighbors in the table
-      #take the current vertice's know cost from the table
-      #add it to the cost of the neighbor from the graph
-      neighbor_name = n[0]
-      print("Neighbor", neighbor_name)
-      neighbor_cost = n[1]
-      current_cost = table[current][0]
-      print("Neighbor cost", neighbor_cost)
-      print("Current cost", current_cost)
-      distance = current_cost + neighbor_cost
-      print("Distance", distance)
 
-      #update the table with cost ONLY if the distance is LESS than the recorded distance in the table for the neighbor
-      cost_in_table = table[neighbor_name][0]
-      if distance < cost_in_table:
-        #update the table
-        table[neighbor_name][0] = distance
-        #update the previous vertex in the table
-        table[neighbor_name][1] = current
+  while current != None:
+      visited = []
+      neighbors = graph[current]
+      print("Current: ", current)
+      print("Neighbors", neighbors)
+      for n in neighbors:
+        if n[0] not in visited:
+          #update costs of neighbors in the table
+          #take the current vertice's know cost from the table
+          #add it to the cost of the neighbor from the graph
+          neighbor_name = n[0]
+          print("Neighbor", neighbor_name)
+          neighbor_cost = n[1]
+          current_cost = table[current][0]
+          print("Neighbor cost", neighbor_cost)
+          print("Current cost", current_cost)
+          distance = current_cost + neighbor_cost
+          print("Distance", distance)
 
+          #update the table with cost ONLY if the distance is LESS than the recorded distance in the table for the neighbor
+          cost_in_table = table[neighbor_name][0]
+          if distance < cost_in_table:
+            #update the table
+            table[neighbor_name][0] = distance
+            #update the previous vertex in the table
+            table[neighbor_name][1] = current
 
 
   #once I'm done looking at all neighbors
-  print(table)
+      print(table)
+      print(unvisited)
+      unvisited.remove(current)
+      if len(unvisited) != 0:
+          current = unvisited[0]
+      else:
+          current = None
 
 
+  # print("current")
+  # print(current)
   #replace current vertex with the lowest cost neighbor
   #look at neighbors not in visited and pick the one with lowest cost in table to look at next
   #initial min = infinity
@@ -79,12 +90,26 @@ def build_shortest_path(graph, start):
   return table
 
 def get_shortest_path(table, start, end):
+    list_of_places = []
+    list_of_places.append(end)
+    current = end
+    while current != start:
+        # table[current]
+        # print(table[current][1])
+        current = table[current][1]
+        list_of_places.append(current)
+        print(list_of_places)
+    list_of_places.reverse()
+    return list_of_places
+
+
+
   #return the shortest path given the table
   #start at the end and keep looking at previous vertices
-  pass
+
 
 example_graph = {"Port Sarim":[("Darkmeyer", 10), ("Al Kharid", 4)], "Darkmeyer":[("Port Sarim", 10), ("Al Kharid", 4), ("Varrock", 3), ("Nardah", 2)], "Al Kharid": [("Port Sarim", 4), ("Darkmeyer", 4), ("Varrock", 2)], "Varrock":[("Al Kharid", 2), ("Darkmeyer", 3), ("Nardah", 3)], "Nardah": [("Varrock", 3), ("Darkmeyer", 2)]}
 
-table = build_shortest_path(example_graph, "Port Sarim")
+table = build_shortest_path(example_graph, "Darkmeyer")
 
-print(get_shortest_path(table, "Port Sarim", "Nardah"))
+print(get_shortest_path(table, "Darkmeyer", "Nardah"))
